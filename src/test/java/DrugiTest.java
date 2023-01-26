66e0a22266849e0b67adb1aea6242d2f3334ab65
RaQ13 <lukasz46@op.pl>
2023-01-26 15:36:47 +0100

    drugi test

---
+++ src/test/java/DrugiTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class DrugiTest {
+
+    @Test
+    public void findSeleniumTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://seleniumdemo.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//a[contains(@href, 'http://seleniumdemo.com/?page_id=4')]//span[text()='Shop']")).click();
+        WebElement seleniumProduct = driver.findElement(By.xpath("//h2[text()='Java Selenium WebDriver']"));
+
+        //sprawdza czy element jest wyświetlony
+        Assert.assertTrue(seleniumProduct.isDisplayed());
+        driver.quit();
+    }
+
+}
