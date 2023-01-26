771013029f6e572b0524b7ea07fff71b5e5938ad
RaQ13 <lukasz46@op.pl>
2023-01-26 10:31:50 +0100

    Firefox test

---
+++ src/test/java/FirefoxTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class FirefoxTest {
+
+    @Test
+    public void openGooglePage() {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.google.com");
+    }
+}
