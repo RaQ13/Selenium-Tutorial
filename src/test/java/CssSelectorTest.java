080faaedc5e2e6cd5e33db6341e01a958daf282d
RaQ13 <lukasz46@op.pl>
2023-01-31 09:50:15 +0100

    wyszukiwanie za pomoca podstawowych selektorow css

---
+++ src/test/java/CssSelectorTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CssSelectorTest {
+
+    @Test
+    public void findElements() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+        By cssId = By.cssSelector("#clickOnMe");
+        driver.findElement(cssId);
+
+        By cssClass = By.cssSelector(".topSecret");
+        driver.findElement(cssClass);
+
+        By tagName = By.cssSelector("input");
+        driver.findElement(tagName).sendKeys("pierwszy element");
+
+        By cssName = By.cssSelector("[name='fname']");
+        driver.findElement(cssName);
+
+        By cssClass2 = By.cssSelector("[class='topSecret']");
+        driver.findElement(cssClass2);
+    }
+}
