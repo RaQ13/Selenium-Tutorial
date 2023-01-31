57ec3997f613778bf1e3f7ad43e1e80f53ced4ec
RaQ13 <lukasz46@op.pl>
2023-01-31 11:30:58 +0100

    zaawansowane selektory css

--- src/test/java/CssSelectorTest.java
+++ src/test/java/CssSelectorTest.java
@@ -24,5 +24,44 @@ public class CssSelectorTest {
 
         By cssClass2 = By.cssSelector("[class='topSecret']");
         driver.findElement(cssClass2);
+
+        /** Część druga po weryfikacji w konsoli za pomocą $$("") */
+
+        By all = By.cssSelector("*");
+        driver.findElements(all);
+
+        By ulInsideDiv = By.cssSelector("div ul");
+        By ulInsideTable = By.cssSelector("table ul");
+        By ulInsideTbody = By.cssSelector("tbody ul");
+
+        driver.findElements(ulInsideDiv);
+        driver.findElements(ulInsideTable);
+        driver.findElements(ulInsideTbody);
+
+        By firstChildUlInDiv = By.cssSelector("div > ul");
+        By firstChildTrInTbody = By.cssSelector("tbody > tr");
+
+        driver.findElements(firstChildUlInDiv);
+        driver.findElements(firstChildTrInTbody);
+
+        By firstFormAferLabel = By.cssSelector("label + form");
+        By allFormsAfterLabel = By.cssSelector("label ~ form");
+
+        driver.findElement(firstFormAferLabel);
+        driver.findElements(allFormsAfterLabel);
+
+        /** cz 3 za pomocą fragmentów ciągów znaków */
+
+        By attrTag = By.cssSelector("input[name='fname']"); // inputy z attr name
+        By attrContains = By.cssSelector("[name*='name']"); // wszystkie zawierajace ciag name
+        By attrStarts = By.cssSelector("[name^='f']"); // zaczynajacę się od f
+        By attrEnds = By.cssSelector("[name$='name']"); // kończy się frazą name
+
+        driver.findElement(attrTag);
+        driver.findElement(attrContains);
+        driver.findElement(attrStarts);
+        driver.findElement(attrEnds);
+
+        driver.quit();
     }
 }
