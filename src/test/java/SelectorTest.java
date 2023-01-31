b8505cc5e5bfa08aa891b47eb63c18a8f9ecc236
RaQ13 <lukasz46@op.pl>
2023-01-31 09:37:12 +0100

    Wyszukiwanie elementow w html

---
+++ src/test/java/SelectorTest.java
@@ -0,0 +1,65 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class SelectorTest {
+
+    @Test
+    public void findElements() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+        /** Lokalizowanie elementów by */
+
+//        By buttonId = By // Tworzy obiekt klasy By, By. pokazuje dostępne możliwe metody lokalizacji
+//        By.id, By.xpath, By.name itd
+//        driver.findElement(); przyjmuje By w parametrze
+
+        By buttonId = By.id("clickOnMe"); // w przypadku literówki test failuje
+        WebElement clickOnMeButton = driver.findElement(buttonId);
+
+        /** Za pomocą name */
+
+        //dłuższy zapis
+        By firstName = By.name("fname");
+        WebElement firstNameInput = driver.findElement(firstName);
+
+        //krótszy
+        driver.findElement(By.name("fname"));
+
+        /** za pomocą wartości atrybutu class */
+
+        //dluższy
+        By hiddenP = By.className("topSecret");
+        driver.findElement(hiddenP);
+
+        //krótszy
+        driver.findElement(By.className("topSecret"));
+
+        /** za pomocą tagu + wszystkie spełniajace kryteria */
+
+        //zwraca 1 element
+        By inputLocator = By.tagName("input");
+        WebElement input = driver.findElement(inputLocator);
+        input.sendKeys("pierwszy input");
+
+        //zwraca listę
+        List<WebElement> inputs = driver.findElements(inputLocator);
+        System.out.println(inputs.size());
+
+        /** za pomoą linku */
+
+        //za pomocą całego tesktu w znaczniku <a>
+        By linkTextLocator = By.linkText("Visit W3Schools.com!");
+        WebElement linkText = driver.findElement(linkTextLocator);
+
+        //za pomocą wycinku tekstu
+        By partalLinkLocator = By.partialLinkText("Visit");
+        WebElement partialLinkText = driver.findElement(partalLinkLocator);
+
+    }
+}
