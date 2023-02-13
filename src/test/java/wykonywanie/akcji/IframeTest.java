a68b7f885ae1eeb6fa9664f6058e6b4f70319d4d
RaQ13 <lukasz46@op.pl>
2023-02-13 12:28:11 +0100

    oblusga iframe cz2

--- src/test/java/wykonywanie/akcji/IframeTest.java
+++ src/test/java/wykonywanie/akcji/IframeTest.java
@@ -2,6 +2,7 @@ package wykonywanie.akcji;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -11,7 +12,10 @@ public class IframeTest {
     public void testIframe() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/iframe.html");
-        driver.switchTo().frame(0); //przepięcie do pierwszego znalezionego iframe
+        WebElement iframe = driver.findElement(By.cssSelector("[src='basics.html']"));
+//        driver.switchTo().frame("wartosć id lub wartosć name");// frame(string)
+//        driver.switchTo().frame(0); //przepięcie do pierwszego znalezionego iframe
+        driver.switchTo().frame(iframe); //przepiecie za pomocą WebElement
         driver.findElement(By.id("fname")).sendKeys("pole tekstowe");
         driver.switchTo().defaultContent(); // powrót do pierwotnego pliku html
         System.out.println(driver.findElement(By.tagName("h1")).getText());
