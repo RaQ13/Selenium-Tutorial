fcb960760b3f6b32c12732d278222a462bcb528e
RaQ13 <lukasz46@op.pl>
2023-02-14 12:21:41 +0100

    klik prawym przyciskiem myszy

---
+++ src/test/java/wykonywanie/akcji/ActionsTest.java
@@ -0,0 +1,21 @@
+package wykonywanie.akcji;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class ActionsTest {
+
+    @Test
+    public void actions() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
+
+        /** klik prawym przyciskiem myszy */
+
+        Actions actions = new Actions(driver);
+        actions.contextClick(driver.findElement(By.id("myFile"))).perform();
+    }
+}
