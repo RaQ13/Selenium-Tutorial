b780c71884a140bcc8b8814de5889e2c2bef0636
RaQ13 <lukasz46@op.pl>
2023-02-16 13:23:16 +0100

    adnotacje @afterTest i @afterMethod

--- src/test/java/testng/BaseTest.java
+++ src/test/java/testng/BaseTest.java
@@ -1,5 +1,7 @@
 package testng;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
@@ -10,8 +12,18 @@ public class BaseTest {
         System.out.println("I am running before test");
     }
 
+    @AfterTest
+    public void afterTest() {
+        System.out.println("I am running after test");
+    }
+
     @BeforeMethod
     public void beforeMethod() {
         System.out.println("I am running before method");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("I am running after method");
     }
 }
