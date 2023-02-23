0e82caa0f08234a3f500dc66e7415b548d59e97a
RaQ13 <lukasz46@op.pl>
2023-02-23 12:57:12 +0100

    data provider

--- src/test/java/testng/adnotacje/FourthTest.java
+++ src/test/java/testng/adnotacje/FourthTest.java
@@ -1,5 +1,6 @@
 package testng.adnotacje;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class FourthTest {
@@ -13,4 +14,14 @@ public class FourthTest {
     public void secondTest() { System.out.println("second test");}
     @Test
     public void thirdTest() { System.out.println("third test");}
+
+    @Test(dataProvider = "data")
+    public void dpTest(String value, String number) {
+        System.out.println(value);
+        System.out.println(number);
+    }
+    @DataProvider(name = "data")
+    public Object[][] dataProvider() {
+        return  new Object[][] {{"I am first test", "First"},{"I am second test", "Second"},{"I am third test", "Third"}};
+    }
 }
